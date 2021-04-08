31832888c3009369e13529c8de91bb1edb2132df
MartinBriand <45079118+MartinBriand@users.noreply.github.com>
2021-04-08 12:24:05 +0200

    Carrier implemented

--- Game/carrier.py
+++ Game/carrier.py
@@ -12,26 +12,50 @@ class Carrier:
             * Or will not get a good and may decide to stay or move to another node
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, in_transit, next_node, time_to_go, load, environment, expenses, revenues):
+        # state: if not in transit, we are at node next_node, ef not time_to_go > 0 and we are going to next_node
+        self.in_transit = in_transit
+        self.next_node = next_node
+        self.time_to_go = time_to_go
+        self.load = load
+        self.environment = environment
+
+        # costs are allowed to be methods
+
+        self.expenses = expenses
+        self.revenues = revenues
+        self.total_expenses = sum(self.expenses)
+        self.total_revenues = sum(self.revenues)
+
+        if not self.in_transit:
+            self.next_node.add_carrier_to_waiting_list(self)
 
     def bid(self):
         """To be called by the node before an auction"""
         raise NotImplementedError
 
-    def get_attribution(self):
+    def get_attribution(self, load, next_node):
         """To be called by the node after an auction if a load was attributed to the carrier"""
-        pass
+        self.in_transit = True
+        current_node = self.next_node
+        self.next_node = next_node
 
-    def receive_payment(self):
+        self.time_to_go = self.environment.get_distance(current_node, self.next_node)
+        self.load = load  # note that the get_attribution of the load is called by the node too.
+
+    def receive_payment(self, value):
         """To be called by the shipper after an auction if a load was attributed"""
-        pass
+        self.revenues.append(value)
+        self.total_revenues += value
 
     def dont_get_attribution(self):
         """To be called by the node after an auction if the carrier lost"""
-        self.next_node = self._decide_next_node()
-        if self.next_node != self.current_node:
-            pass
+        new_next_node = self._decide_next_node()
+        if new_next_node != self.next_node:
+            self.in_transit = True
+            current_node = self.next_node
+            self.next_node = new_next_node
+            self.time_to_go = self.environment.get_distance(current_node, self.next_node)
 
     def _decide_next_node(self):
         """Decide of a next node after losing an auction (can be the same node when needed)"""
@@ -39,8 +63,26 @@ class Carrier:
 
     def next_step(self):
         """To be called by the environment at each iteration"""
-        pass
+        if self.in_transit:
+            self.time_to_go -= 1
+            new_cost = self._transit_costs() + self._far_from_home_costs()
+            if self.time_to_go == 0:
+                self._arrive_at_next_node()
+        else:
+            new_cost = self._far_from_home_costs()
+
+        self.expenses.append(new_cost)
+        self.total_expenses += new_cost
 
     def _arrive_at_next_node(self):
         """Called by next_step to do all the variable settings when arrive at a next node"""
-        pass
+        self.in_transit = False
+        self.load.arrive_at_next_node()
+        self.load = None
+        self.next_node.add_carrier_to_waiting_list(self)  # TODO: Is this implemented in the node API
+
+    def _transit_costs(self):
+        raise NotImplementedError
+
+    def _far_from_home_costs(self):
+        raise NotImplementedError
