c843d7593d2979b39563b9132a3b272bc0ab60a2
MartinBriand <45079118+MartinBriand@users.noreply.github.com>
2021-04-22 18:24:09 +0200

    Development finished

--- Mechanics/Actors/carriers/learning_carrier.py
+++ Mechanics/Actors/carriers/learning_carrier.py
@@ -25,12 +25,16 @@ During exploitation
 # TODO: is this description correct at the end of the implementation?
 
 from random import random
-from tensorflow import constant as tf_constant, concat as tf_concat
+from tensorflow import constant as tf_constant, concat as tf_concat, Variable
 from tensorflow.python.framework.ops import EagerTensor
 from tf_agents.agents.td3.td3_agent import Td3Agent
+from tf_agents.networks import Network
+from tf_agents.replay_buffers.replay_buffer import ReplayBuffer
 from tf_agents.trajectories.policy_step import PolicyStep
 from tf_agents.trajectories.time_step import TimeStep, StepType
-from tf_agents.trajectories.trajectory import mid
+from tf_agents.trajectories.trajectory import Transition
+
+import tf_agents.typing.types as tfa_types
 
 from Mechanics.Actors.carriers.carrier import CarrierWithCosts
 
@@ -68,9 +72,10 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
                  time_not_at_home: int,
                  learning_agent: 'LearningAgents',
                  is_learning: bool,
-                 gamma: float,
+                 discount: float,
+                 discount_power: Optional[int],
                  time_step: Optional[TimeStep],
-                 action: Optional[PolicyStep]) -> None:  # TODO this function is useless for the moment
+                 policy_step: Optional[PolicyStep]) -> None:
 
         super().__init__(name=name,
                          home=home,
@@ -88,12 +93,13 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
                          time_not_at_home=time_not_at_home)
 
         self._learning_agent: 'LearningAgents' = learning_agent
+        self._buffer: ReplayBuffer = self._learning_agent.replay_buffer
 
-        if gamma > 1 or gamma < 0:
-            raise ValueError('Gamma between 0 and 1')
+        if discount > 1 or discount < 0:
+            raise ValueError('Discount between 0 and 1')
 
-        self._gamma: EagerTensor = tf_constant([gamma])
-        self._gamma_power: int = 1
+        self._discount: EagerTensor = tf_constant([discount])
+        self._discount_power: int = discount_power if discount_power else 1
 
         self._is_learning: bool = is_learning
         if self._is_learning:
@@ -101,17 +107,17 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
         else:
             self._policy = self._learning_agent.policy
 
-        if time_step:
+        self._is_first_step = time_step is None
+        if not self._is_first_step:
             self._time_step: Optional[TimeStep] = time_step
-        else:
+        elif not self._in_transit:
             self._time_step: Optional[TimeStep] = self._generate_current_time_step()
 
-        if action:
-            self._action_step: Optional[PolicyStep] = action
+        if policy_step:
+            self._policy_step: Optional[PolicyStep] = policy_step
         else:
-            self._action_step: Optional[PolicyStep] = None
+            self._policy_step: Optional[PolicyStep] = None
 
-    # Carrier's methods
     def _decide_next_node(self) -> 'Node':  # Very simple function to get back home in 20% of the lost auctions
         # This could get smarter later
         """Decide of a next nodes after losing an auction (can be the same nodes when needed)"""
@@ -123,18 +129,18 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
 
     def get_attribution(self, load: 'Load', next_node: 'Node') -> None:
         super().get_attribution(load, next_node)
-        self._gamma_power = self._time_to_go
+        self._discount_power = self._time_to_go
 
     def dont_get_attribution(self) -> None:
         super().dont_get_attribution()
         if self._in_transit:
-            self._gamma_power = self._time_to_go
+            self._discount_power = self._time_to_go
         else:
-            self._gamma_power = 1
+            self._discount_power = 1
 
-    def bid(self, node: 'Node') -> 'CarrierBid':  # TODO
-        self._action_step = self._policy.action(self._time_step)  # the time step is generated in next_step
-        action = self._action_step.action.numpy()
+    def bid(self, node: 'Node') -> 'CarrierBid':
+        self._policy_step = self._policy.action(self._time_step)  # the time step is generated in next_step
+        action = self._policy_step.action.numpy()
         node_list = self._environment.nodes
         bid = {}
         for k in range(action.shape[0]):
@@ -144,35 +150,46 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
     def set_new_cost_parameters(self, t_c: float, ffh_c: float) -> None:
         self._t_c = t_c
         self._ffh_c = ffh_c
+        self._discount_power = 1
+        self._is_first_step = True
 
     def next_step(self) -> None:
         super().next_step()
-        if not self._in_transit and self._is_learning:
-            new_time_step = self._generate_current_time_step()
-            if self._is_learning:
-                self._generate_trajectory(new_time_step)
-            self._time_step = new_time_step
+        if not self._in_transit:
+            next_time_step = self._generate_current_time_step()
+            if self._is_learning and not self._is_first_step:
+                self._generate_transition(next_time_step)
+            self._time_step = next_time_step
 
-    def _generate_trajectory(self, new_time_step: TimeStep) -> None:  # TODO
-        if self._action_step:
-            trajectory = mid(observation=self._time_step.observation,
-                             action=self._action_step.action,
-                             policy_info=self._action_step.info,
-                             reward=new_time_step.reward,
-                             discount=self._gamma ** self._gamma_power)  # note that reward is update in next_step()
+    def _generate_transition(self, next_time_step: TimeStep) -> None:
+        if self._policy_step:
+            transition = Transition(time_step=self._time_step,
+                                    action_step=self._policy_step,
+                                    next_time_step=next_time_step)
+
+            # note that reward is update in next_step()
             # communicate trajectory
-            # TODO: How do I know the next state in the trajectory?
+            self._buffer.add_batch(transition)
 
     def _generate_current_time_step(self) -> TimeStep:
-        node_state = self._environment.this_node_state(self._next_node)
+        node_state = self._environment.this_node_state(self._next_node)  # silent the pycharm error
         cost_state = tf_constant([self._t_c, self._ffh_c, self._time_not_at_home])
+        observation = tf_concat([node_state, cost_state], 0)
+        discount = self._discount ** self._discount_power
+        if self._is_first_step:
+            step_type = StepType.FIRST
+            reward = tf_constant([0])
+        else:
+            step_type = StepType.MID
+            reward = tf_constant([self._episode_revenues[-1] - self._episode_expenses[-1]])
 
         # note that reward is update in next_step()
-        # and that gamma_power is updated in the _get_attribution()/_dont_get_attribution()
-        return TimeStep(step_type=StepType.MID,
-                        reward=tf_constant([self._episode_revenues[-1] - self._episode_expenses[-1]]),
-                        discount=self._gamma ** self._gamma_power,
-                        osbervation=tf_concat([node_state, cost_state], 0))
+        # and that discount_power is updated in the _get_attribution()/_dont_get_attribution()
+        self._is_first_step = False
+        return TimeStep(step_type=step_type,
+                        reward=reward,
+                        discount=discount,
+                        osbervation=observation)
 
     @property
     def is_learning(self):
@@ -187,5 +204,63 @@ class LearningCarrier(CarrierWithCosts):  # , TFEnvironment):
             self._policy = self._learning_agent.policy
 
 
+# here we can silent the pycharm error
 class LearningAgents(Td3Agent):  # TODO: implement this
-    pass
+    # TODO write description
+    def __init__(self,
+                 replay_buffer: ReplayBuffer,
+                 time_step_spec: TimeStep,
+                 action_spec: tfa_types.NestedTensor,
+                 actor_network: Network,
+                 critic_network: Network,
+                 actor_optimizer: tfa_types.Optimizer,
+                 critic_optimizer: tfa_types.Optimizer,
+                 exploration_noise_std: tfa_types.Float = 0.1,
+                 critic_network_2: Optional[Network] = None,
+                 target_actor_network: Optional[Network] = None,
+                 target_critic_network: Optional[Network] = None,
+                 target_critic_network_2: Optional[Network] = None,
+                 target_update_tau: tfa_types.Float = 1.0,
+                 target_update_period: tfa_types.Int = 1,
+                 actor_update_period: tfa_types.Int = 1,
+                 td_errors_loss_fn: Optional[tfa_types.LossFn] = None,
+                 gamma: tfa_types.Float = 1.0,
+                 reward_scale_factor: tfa_types.Float = 1.0,
+                 target_policy_noise: tfa_types.Float = 0.2,
+                 target_policy_noise_clip: tfa_types.Float = 0.5,
+                 gradient_clipping: Optional[tfa_types.Float] = None,
+                 debug_summaries: bool = False,
+                 summarize_grads_and_vars: bool = False,
+                 train_step_counter: Optional[Variable] = None,
+                 name: tfa_types.Text = None) -> None:
+
+        super().__init__(time_step_spec=time_step_spec,
+                         action_spec=action_spec,
+                         actor_network=actor_network,
+                         critic_network=critic_network,
+                         actor_optimizer=actor_optimizer,
+                         critic_optimizer=critic_optimizer,
+                         exploration_noise_std=exploration_noise_std,
+                         critic_network_2=critic_network_2,
+                         target_actor_network=target_actor_network,
+                         target_critic_network=target_critic_network,
+                         target_critic_network_2=target_critic_network_2,
+                         target_update_tau=target_update_tau,
+                         target_update_period=target_update_period,
+                         actor_update_period=actor_update_period,
+                         td_errors_loss_fn=td_errors_loss_fn,
+                         gamma=gamma,
+                         reward_scale_factor=reward_scale_factor,
+                         target_policy_noise=target_policy_noise,
+                         target_policy_noise_clip=target_policy_noise_clip,
+                         gradient_clipping=gradient_clipping,
+                         debug_summaries=debug_summaries,
+                         summarize_grads_and_vars=summarize_grads_and_vars,
+                         train_step_counter=train_step_counter,
+                         name=name)
+
+        self._replay_buffer: ReplayBuffer = replay_buffer
+
+    @property
+    def replay_buffer(self):
+        return self._replay_buffer
